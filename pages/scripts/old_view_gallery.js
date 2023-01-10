013fa7cb497e4cdcae9d214f5c8ab9927e8c4312
Joseph <59386472+pixel-streamer@users.noreply.github.com>
2023-01-10 02:37:59 -0600

    researching resize responsive, using css.

--- pages/scripts/old_view_gallery.js
+++ pages/scripts/old_view_gallery.js
@@ -72,60 +72,58 @@
 */
 //define these in the obj, until they can be loaded via xml (as well as the rest)
 //let simpleGalleryConfig = new SimpleGalleryConfig();
-var canvas, stage;
+var canvasHome, canvas, stage;
+var aspect, originW, originH;
+var w = window.innerWidth;
+var h = window.innerHeight;
 
-function init() {
-    canvas = document.getElementById("testCanvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    //stage = new createjs.StageGL(canvas, {});
-    stage = new createjs.Stage(canvas, {});
+let resizeObserver;
+let delay = 250;
+let timeout;
 
+function setupStage() {
+    canvasHome = document.querySelector("#testCanvas");
+    var canvasFrag = document.createDocumentFragment();
+    canvas = document.createElement("canvas");
+    canvasFrag.appendChild(canvas);
+    canvasHome.appendChild(canvasFrag);
+    originW = parseInt(getComputedStyle(canvasHome).width);
+    originH = parseInt(getComputedStyle(canvasHome).height);
+
+    canvas.setAttribute("width", originW);
+    canvas.setAttribute("height", originH);
+    canvas.setAttribute("id", "main_canvas");
+    //maybe setting the styles within the canvas container ARE NOT RIGHT
+    //it seems to work with the styles removed.
+    //TODO: set up a redraw for the screen on resize.
+    /*  canvas.setAttribute(
+        "style",
+        "width:" + originW + "px;" + "height:" + originH + "px;"
+    ); */
+    stage = new createjs.Stage("main_canvas");
+    stage.enableMouseOver();
+    stage.setBounds(0, 0, originW, originH);
+    /*  stage.canvas.width = stage.getBounds().width;
+    stage.canvas.height = stage.getBounds().height; */
+
+    //straight from https://codepen.io/createjs/pen/dZvVKp for testing
+    center = makeCorner();
     topLeft = makeCorner();
     topRight = makeCorner();
     bottomLeft = makeCorner();
     bottomRight = makeCorner();
-    center = makeCorner();
 
     stage.addChild(topLeft, topRight, bottomLeft, bottomRight, center);
+    //ENDs resize test-- debounced.
+    resizeObserver = new ResizeObserver((entries) => {});
 
-    window.addEventListener("resize", resize);
-    resize();
-}
+    resizeObserver.observe(canvas);
 
-function resize() {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-
-    canvas.width = w;
-    canvas.height = h;
-
-    layout(w, h);
-
-  //  stage.updateViewport(w, h);
-    stage.update();
-}
-
-function layout(w, h) {
-    topLeft.x = bottomLeft.x = 0;
-    topRight.x = bottomRight.x = w;
-
-    topLeft.y = topRight.y = 0;
-    bottomLeft.y = bottomRight.y = h;
-
-    center.x = w / 2;
-    center.y = h / 2;
-}
-
-function makeCorner() {
-    var r = 42;
-    var item = new createjs.Shape();
-    item.graphics
-        .beginFill("#4ACFF1")
-        .drawRoundRect(-r, -r, r * 2, r * 2, r * -0.8)
-        .endFill();
-    item.cache(-r, -r, r * 2, r * 2);
-    return item;
+    window.addEventListener("resize", function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(handle_Redraw, delay);
+        return;
+    });
 }
 
 //I think that createjs requires the starting function to be "init"
@@ -166,3 +164,57 @@ function handle_Click() {
 //window.addEventListener();
 
 window.addEventListener("load", init);
+
+function init() {
+    setupStage();
+}
+
+function layout(w, h) {
+    topLeft.x = bottomLeft.x = 0;
+    topRight.x = bottomRight.x = w;
+
+    topLeft.y = topRight.y = 0;
+    bottomLeft.y = bottomRight.y = h;
+
+    center.x = parseInt(w / 2);
+    center.y = parseInt(h / 2);
+}
+
+function makeCorner() {
+    //from https://codepen.io/createjs/pen/dZvVKp
+    var r = 32;
+    var item = new createjs.Shape();
+    item.graphics
+        .beginFill("#4ACFF1")
+        .drawRoundRect(-r, -r, r * 2, r * 2, r * -0.88)
+        .endFill();
+    item.cache(-r, -r, r * 2, r * 2);
+    return item;
+}
+
+function handle_Redraw() {
+    console.log("you resized me.....");
+
+    /*
+        //should this use offset width and height?
+        w = canvasHome.clientWidth;
+        h = canvasHome.clientHeight;  
+        w = window.innerWidth;
+        h = window.innerHeight;
+    */
+    //I'm going to let css tell us the correct box height of the main container.
+    w = Math.max(parseInt(getComputedStyle(canvasHome).width), 320);
+    h = Math.max(parseInt(getComputedStyle(canvasHome).height), 320);
+    console.log("::: handle_Redraw :::", w, h);
+
+    stage.canvas.width = w;
+    stage.canvas.height = h;
+
+    /*  aspect = Math.min(originW, originH) / Math.max(originW, originH);
+    stage.canvas.width = w * aspect;
+    stage.canvas.height = h * aspect;
+    console.log("::: aspect :::", aspect, w * aspect, h * aspect);
+    layout(canvas.width, canvas.height); */
+    layout(w, h);
+    stage.update();
+}
