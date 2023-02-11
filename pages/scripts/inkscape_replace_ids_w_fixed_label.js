644f69118feeeeebe98b28e354e4858a0cbfc3e0
Joseph <59386472+pixel-streamer@users.noreply.github.com>
2023-02-11 02:08:26 -0600

    added the "original", "served svg, not local fix" version of the "fix" code
    fix was found here originally:
    https://inkscape.org/forums/questions/naming-the-ids-of-layers-for-svg-exporting/

--- pages/scripts/inkscape_replace_ids_w_fixed_label.js
+++ pages/scripts/inkscape_replace_ids_w_fixed_label.js
@@ -6,17 +6,19 @@
             var id = label;
             el.setAttribute("id", id);
             el.setAttribute("inkscape:label", label);
-            /*
-			console.log(
-                "label: ",
-                el.getAttribute("inkscape:label"),
-                "id: ",
-                el.getAttribute("id")
-            );
-			*/
+             
+			//console.log(
+            //    "label: ",
+            //    el.getAttribute("inkscape:label"),
+            //    "id: ",
+            //    el.getAttribute("id")
+            //);
+			 
             return el;
         } else {
             return;
         }
     });
-})();
+})(); 
+//this was listed as the correct solution, but I couldn't get it work when viewing the svg locally :
+//document.querySelectorAll("g[*|groupmode='layer']").forEach(el => el.id = el.getAttributeNS('http://www.inkscape.org/namespaces/inkscape', 'label').replace(/\W/g, '_'))
