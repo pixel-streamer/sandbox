75e9038661f86d98ee071a90200d2a2e7e0328d0
Joseph <59386472+pixel-streamer@users.noreply.github.com>
2023-02-14 13:13:07 -0600

    Update sounds_from_a_list.js

--- pages/rocket_ranger_sounds/scripts/sounds_from_a_list.js
+++ pages/rocket_ranger_sounds/scripts/sounds_from_a_list.js
@@ -425,7 +425,7 @@ function makeSomeText() {
     var text = new createjs.Text(
         "WHO IS THIS GUY?",
         //"16px 'Press Start 2P'",
-        "800 64px 'Barlow ExtraBold'",
+        "italic 64px 'Bungee'",
         // "#ff7700"
         "#FFFFFF"
     );
