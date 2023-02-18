c645c3f3f13c9aa09cd6b6cd52699d7bcad85b14
Joseph <59386472+pixel-streamer@users.noreply.github.com>
2023-02-18 01:32:11 -0600

    added all cards image

---
+++ pages/rocket_ranger_sounds/scripts/cards_load.js
@@ -0,0 +1,434 @@
+var stage,
+    stageBounds,
+    importantVideo,
+    fontsHaveLoaded = false,
+    ticker,
+    subject_content,
+    image_content,
+    startup_content,
+    fileLoader,
+    bigArea = document.querySelector("#testCanvas"),
+    w = parseInt(getComputedStyle(bigArea).width),
+    h = parseInt(getComputedStyle(bigArea).height),
+    bigCanvas = document.querySelector(".full_size_canvas"),
+    generalPadding = 16,
+    largeText = 0;
+
+let nofullhover;
+
+let resizeObserver,
+    delay = 250,
+    timeout;
+
+let screenLog = document.querySelector("#screen-log");
+/*
+ResizeObserver.disconnect()
+    Unobserves all observed Element targets of a particular observer.
+ResizeObserver.observe()
+    Initiates the observing of a specified Element.
+ResizeObserver.unobserve()
+    Ends the observing of a specified Element.
+*/
+window.addEventListener("load", loadGoogleFonts);
+/* 
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ FONT LOAD FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+// import { loadGoogleFonts }
+// import "font-loading_module.js";
+
+function loadFonts(config) {
+    var loader = new createjs.FontLoader(config, true);
+    loader.on("complete", handleFontLoad);
+    loader.load();
+}
+
+const fontload_evt = new CustomEvent("fontload_evtStr", {
+    detail: { msg: ":::fontloaded, now setup stage" },
+});
+
+function handleFontLoad(e) {
+    fontsHaveLoaded = true;
+    window.dispatchEvent(fontload_evt);
+}
+
+window.addEventListener("fontload_evtStr", setupStageForInteraction);
+
+/* 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ END OF FONT LOAD FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+*/
+/* 
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ STAGE SETUP FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+const startup_evt = new CustomEvent("startup_evtStr", {
+    detail: { msg: ":::startup began, now setup stage" },
+});
+window.addEventListener("startup_evtStr", addStartupText);
+
+const gameSetup_evt = new CustomEvent("gameSetup_evtStr", {
+    detail: { msg: ":::startup began, now setup stage" },
+});
+window.addEventListener("gameSetup_evtStr", setupGame);
+
+const gamePlay_evt = new CustomEvent("gamePlay_evtStr", {
+    detail: { msg: ":::startup began, now setup stage" },
+});
+window.addEventListener("gamePlay_evtStr", playGame);
+
+function setupStageForInteraction() {
+    init();
+    bigCanvas.setAttribute("width", w);
+    bigCanvas.setAttribute("height", h);
+    stage = bigCanvas;
+    stage = new createjs.Stage("big_stage");
+    stage.setBounds(0, 0, w, h);
+    stageBounds = stage.getBounds();
+
+    startup_content = new createjs.Container();
+    startup_content.name = "startup_content";
+
+    stage.addChild(startup_content);
+
+    ticker = createjs.Ticker;
+    ticker.timingMode = ticker.RAF_SYNCHED;
+    ticker.addEventListener("tick", tick);
+    //nofullhover = window.matchMedia("(hover:none), (hover:on-demand)").matches;
+    interactive_content = new createjs.Container();
+    interactive_content.name = "interactive_content";
+
+    image_content = new createjs.Container();
+    image_content.name = "image_content";
+
+    subject_content = new createjs.Container();
+    subject_content.name = "subject_content";
+    stage.addChildAt(subject_content);
+    stage.addChildAt(image_content);
+    stage.addChild(interactive_content);
+
+    addStartupText();
+}
+
+function tick(event) {
+    stage.update(event);
+}
+
+/* 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ END OF STAGE SETUP FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+*/
+
+/* 
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ RESIZE FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+
+resizeObserver = new ResizeObserver((entries) => {});
+resizeObserver.observe(document.querySelector("#testCanvas"));
+window.addEventListener("resize", function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(handle_Redraw, delay);
+    return;
+});
+
+function handle_Redraw() {
+    // console.log(
+    //     "▄▄▄▄▄▄▄▄▄handle_Redraw▄▄▄▄▄▄▄▄",
+    //     "find a way to add something to all corners of the stage, and re-dim that sucker"
+    // );
+}
+/* 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ END OF RESIZE FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+*/
+
+function init() {
+    // console.log("▄▄▄▄▄▄▄▄▄init▄▄▄▄▄▄▄▄");
+}
+
+/* 
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ IMAGE LOAD FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+function addStartupText() {
+    console.log("addStartupText");
+    var nextLargerTextSize = getGoldenRatio(w) * 0.04;
+    var largerTextContainer = new createjs.Container();
+
+    var largerText = new createjs.Text(
+        "click to begin".toUpperCase(),
+        "normal " + nextLargerTextSize + "px 'Press Start 2P'",
+        "#ffffff"
+    );
+
+    var largerTextMetrics = largerText.getMetrics();
+
+    var textClickArea = new createjs.Shape();
+    textClickArea.graphics
+        .beginFill("#450067")
+        .drawRect(
+            0,
+            0,
+            largerTextMetrics.width + 32,
+            largerTextMetrics.height + 32
+        )
+        .endFill();
+    largerTextContainer.addChild(textClickArea);
+    largerTextContainer.addChild(largerText);
+    var largerTextW = largerTextMetrics.width;
+    var largerTextH = largerTextMetrics.height;
+
+    largerText.x = (stageBounds.width - largerTextMetrics.width) / 2;
+    largerText.y = largerTextMetrics.height / 2;
+
+    textClickArea.x = (stageBounds.width - (largerTextMetrics.width + 32)) / 2;
+    textClickArea.y = largerText.y - (largerTextMetrics.height + 24) / 2;
+    largerTextContainer.y = 0;
+    interactive_content.addChild(largerTextContainer);
+
+    largerTextContainer.addEventListener(
+        "click",
+        function () {
+            largerTextContainer.visible = false;
+            largerTextContainer.mouseEnabled = false;
+            window.dispatchEvent(gameSetup_evt);
+        },
+        { once: true }
+    );
+}
+
+function addInteractiveText(interactivePhrase) {
+    largeText = getGoldenRatio(w) * 0.085;
+    var interactiveTextHitArea = new createjs.Container();
+    var interactiveTextMask = new createjs.Shape();
+
+    var gamePlayText = new createjs.Text(
+        interactivePhrase,
+        "16px 'Press Start 2P'",
+        "#FFCC00"
+    );
+    var textMetrics = gamePlayText.getMetrics();
+    var textW = textMetrics.width;
+    var textH = textMetrics.height;
+    gamePlayText.x = 8;
+    gamePlayText.y = 8;
+
+    interactiveTextMask.graphics
+        .beginFill("rgba(0,0,0,.3)")
+        .drawRect(0, 0, textW + 16, textH + 16)
+        .endFill();
+
+    interactiveTextHitArea.regX = 0;
+    interactiveTextHitArea.regY = 0;
+
+    interactiveTextHitArea.addChild(interactiveTextMask);
+    interactiveTextHitArea.addChild(gamePlayText);
+
+    interactiveTextHitArea.x =
+        (stageBounds.width - interactiveTextHitArea.getBounds().width) / 2;
+    interactiveTextHitArea.y =
+        (stageBounds.height - interactiveTextHitArea.getBounds().height) / 2;
+
+    interactiveTextHitArea.addEventListener(
+        "click",
+        function () {
+            console.log("clicked me one time!");
+            interactiveTextHitArea.visible = false;
+            interactiveTextHitArea.mouseEnabled = false;
+            window.dispatchEvent(gamePlay_evt);
+        },
+        { once: true }
+    );
+
+    // handle_SoundsRegistry();
+    interactive_content.addChild(interactiveTextHitArea);
+}
+
+/* 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ END OF IMAGE LOAD FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+*/
+/* 
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ UTILITY FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+
+function getRandomHexNum() {
+    // get a random hex value for the color of something:
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function getGoldenRatio(num) {
+    /*
+    sin(54°) = φ/2
+
+    Golden ratio formula is ϕ = 1 + (1/ϕ). ϕ is also equal to 2 × sin (54°)
+    If we take any two successive Fibonacci Numbers, their ratio is very close
+    to the value 1.618 (Golden ratio).  cos(36°)*2
+    From en.wikipedia.org/wiki/Golden_ratio#Alternative_forms :
+    "These correspond to the fact that the length of the diagonal of a regular
+    pentagon is φ times the length of its side" 
+    */
+    return parseFloat(num / 1.618).toPrecision(3);
+}
+
+function resizeToKnownDimensions(contentW, contentH, constraintW, constraintH) {
+    var containerAspect = constraintW / constraintH;
+
+    var fullW = contentW;
+    var fullH = contentH;
+
+    var aspect = fullW / fullH;
+    var imageAspect;
+
+    var fullMax = Math.max(fullW, fullH);
+    var fullMin = Math.min(fullW, fullH);
+
+    var contentMax = Math.max(constraintW, constraintH);
+    var contentMin = Math.min(constraintW, constraintH);
+
+    let newScaleRatio;
+
+    imageAspect = aspect <= 1 ? "portrait" : "landscape";
+
+    if (aspect < 1 || aspect === 1) {
+        //use contentMin/fullMax
+        newScaleRatio = contentMin / fullMax;
+        contentW = fullW * newScaleRatio;
+        contentH = fullH * newScaleRatio;
+    } else if (aspect > 1) {
+        //use contentMax/fullMax unless the containerAspect is greater than aspect
+        var greater = Math.max(aspect, containerAspect);
+        if (greater === aspect) {
+            //console.log("greater.... ASPECT!");
+            newScaleRatio = contentMax / fullMax;
+            contentW = fullW * newScaleRatio;
+            contentH = fullH * newScaleRatio;
+        } else {
+            //console.log("greater.... CONTAINERASPECT!");
+            newScaleRatio = contentMin / fullMin;
+            contentW = fullW * newScaleRatio;
+            contentH = fullH * newScaleRatio;
+        }
+    }
+    return {
+        imageAspect: imageAspect,
+        aspect: containerAspect,
+        scaleRatio: newScaleRatio,
+        newW: contentW,
+        newH: contentH,
+    };
+}
+/* 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ END OF UTILITY FUNCTIONS ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+*/
+function setupGame() {
+    console.log("setupGame");
+    var phraseAsStr = "Welcome to Cards.\n\t\t\tLet's Play.";
+    addInteractiveText(phraseAsStr);
+}
+function playGame() {
+    console.log("playGame");
+    fileLoader = new createjs.LoadQueue(true);
+    fileLoader.on("fileload", handle_ImageLoadReady);
+    // fileLoader.on("progress", handle_ImageLoadProgress);
+    fileLoader.on("complete", handle_ImageLoadComplete);
+    fileLoader.loadFile({
+        src: "../images/ui_vectors/cards_all-copy.svg",
+        id: "all_cards",
+        type: createjs.Types.IMAGE,
+    });
+    displaySingleCard(getSuitCode("hearts"));
+}
+
+function handle_ImageLoadReady(e) {
+    //console.log(":::::▄█ handle_ImageLoadReady █▄", e.item.type);
+
+    var src = e.item.src;
+    var extension_trim = src.substring(src.lastIndexOf("/") + 1);
+    var extension = extension_trim.substring(
+        extension_trim.lastIndexOf(".") + 1
+    );
+
+    switch (extension) {
+        // case "svg":
+        //     addSVG(e);
+        //     break;
+        // case "mp4":
+        //     handleLoadedMovie(e);
+        //     break;
+        default:
+            layoutImage(e);
+            break;
+    }
+}
+
+function handle_ImageLoadComplete() {
+    console.log("handle_ImageLoadComplete");
+}
+var galleryConfig = {};
+galleryConfig.thumbnailW = 85;
+galleryConfig.thumbnailH = 64;
+
+function layoutImage(e) {
+    var bg = new createjs.Bitmap(e.result);
+    // bg.scaleX = galleryConfig.thumbnailW / e.result.width;
+    // bg.scaleY = galleryConfig.thumbnailH / e.result.height;
+
+    //add the images to the content
+    image_content.addChild(bg);
+
+    var fsBiggest = resizeToKnownDimensions(
+        e.result.width,
+        e.result.height,
+        w,
+        h
+    );
+    bg.scaleX = fsBiggest.scaleRatio;
+    bg.scaleY = fsBiggest.scaleRatio;
+}
+function loadCards() {
+    console.log("load in some cards");
+}
+
+function displaySingleCard() {
+    console.log("display single card");
+}
+
+function getSuitCode(suit) {
+    console.log("getSuitCode");
+    var symbol = suit.toLowerCase();
+    var choice;
+    switch (symbol) {
+        case "spades":
+            //♠262
+            choice = "♠";
+            break;
+        case "hearts":
+            //♥259
+            choice = "♥";
+            break;
+        case "clubs":
+            //♣261
+            choice = "♣";
+            break;
+        case "diamods":
+            //♦260
+            choice = "♦";
+            break;
+        default:
+            //
+            break;
+    }
+    return choice;
+
+    /*
+·250 ■254 ☺257 ☻258 ♥259 ♦260 ♣261 ♠262 •263 ◘264
+○265 ◙266 ♂267 ♀268 ♪269 ♫270 ☼271 ►272 ◄273 ↕274
+‼275 ¶276 §277 ▬278 ↨279 ↑280 ↓281 →282 ←283 ↔285
+▲286 ▼287 ▲30 ▼31 ·0183 ∙249 °248 “0147 ”0148
+•0149 –0150 —0151 ™0153 ©0169 ¯0175 ‘0145 ’0146 
+*/
+}
